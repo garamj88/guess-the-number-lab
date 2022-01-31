db343bfe952a3a01edf8d3752814aea45165e0ec
Garam Jang <garamj88@gmail.com>
2022-01-31 18:32:32 -0500

    fix play fuction to call render

--- app.js
+++ app.js
@@ -21,12 +21,14 @@ const game = {
 
     // 4. invoke getGuess from inside a loop, and add the new guess to the prevGusses array 
     do {
-      this.prevGuess.push(this.getGuess());
+      this.prevGuesses.push(this.getGuess());
+      
+      // call reander mothod to render the result of the guess
+      this.render();
     } 
     // while guess is not the secret number;
-    while (this.prevGuesses[this.prevGuess.length - 1]!== this.secretNum);
-  
-  },
+    while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum);
+    },
 
   // 2. Add a `getGuess` method to `game` that prompts the player to enter a guess with a message formatted as: "Enter a guess between [smallestNum] and [biggestNum]."
 /** Ensure that the `getGuess` method returns a value that:
@@ -38,49 +40,55 @@ const game = {
   getGuess: function() {
     
     // create a variable (=guess)
-    let guess = 0;
+    let guess = null;
     
     // assign guess as an integer pasred (parseInt) from the guess input
      // prompt will keep popping until the guess matches the secretNum
     
     do {
       // parseInt` returns `NaN` if the string cannot be parsed into a number
+        // while the guess is not a number or guess isn't between min and max
       guess = parseInt(
-        prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`));
-    }
-    
-    // while the guess is not a number or guess isn't between min and max
-    while (
+        prompt(
+          `Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`
+          )
+        );
+    } while (
       isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum
-    )
+    );
 
     return guess;
   },
 
-  // 5. Add a `render` method to `game` that `play` will call after a guess has been made that alerts:
+  // 5. Add a 'render' method to `game` that `play` will call after a guess has been made that alerts:
 
   render: function() {
 
   // - If the secretNum is the same as guess: `Congrats! You guessed the number in [number of prevGuesses]!`
     let promptMsg = null;
 
-    if (this.prevGuess[this.prevGuesses.length -1] === this.secretNum && this.prevGuesses.length <= 1) {
-      promptMsg = `Congrats! You guessed the number in ${this.prevGuesses.length} of guess!` // if number of the guess is singular
-    } else if (this.prevGuess[this.prevGuesses.length -1] === this.secretNum && this.prevGuesses.length > 1) {
-      promptMsg = `Congrats! You guessed the number in ${this.prevGuesses.length} of guesses!` // if number of the guess is plural
-    } else { 
+    if (this.prevGuesses[this.prevGuesses.length -1] === this.secretNum && this.prevGuesses.length <= 1) {
+      promptMsg = `Congrats! You guessed the number in ${this.prevGuesses.length} guess!`; // if number of the guess is singular
+    } else if (this.prevGuesses[this.prevGuesses.length -1] === this.secretNum && this.prevGuesses.length > 1) {
+      promptMsg = `Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`; // if number of the guess is plural
+    } else {
       // - Otherwise: `Your guess is too [high|low] Previous guesses: x, x, x, x`
         // - Template literals not only have interpolation, but they also honor whitespace - including line breaks!
-      promptMsg = `Your guess is too ${this.prevGuess[this.prevGuesses.length -1] > this.secretNum? 'high' : 'low'}\n Previous guesses: ${this.prevGuesses.join(', ')}`;
-    }
+      promptMsg = `Your guess is too ${this.prevGuesses[this.prevGuesses.length -1] > this.secretNum ? 'high' : 'low'}\nPrevious guesses: ${this.prevGuesses.join(', ')}`;
+    };
   
-  // prompt an alert message box
-  alert(promptMsg);
+    // prompt an alert message box
+    alert(promptMsg);
   }
-  //     - The list of previous guesses can be generated using the array `join` method.
+  // - The list of previous guesses can be generated using the array `join` method.
+};
 
-}
+// ## Bonus
 
-game.play()
+// - If the player enters a number greater than the `secretNum` make it the new `biggestNum`, so that the player can't enter a number greater than it. If the player enters a number that is less than the `secretNum` make it the new `smallestNum`, so that the player can't enter a number less than it. Hint: You may want to add a helper function to accomplish this.
 
+// ## More Bonus
 
+// When `play` is run, immediately prompt the player to enter the smallest and biggest numbers instead of pre-setting values.
+
+game.play();
